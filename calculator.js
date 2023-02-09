1e3dc62d62facafad8d8c9ccd812ab254e22f9e2
HowardWillim <kurolimzz@gmail.com>
2023-02-09 11:06:27 +0700

    adding BMI Calculator

--- calculator.js
+++ calculator.js
@@ -8,6 +8,17 @@ app.get("/",function(req,res){
     res.sendFile(__dirname+"/index.html")
 })
 
+app.get("/bmiCalculator",function(req,res){
+    res.sendFile(__dirname+"/bmiCalculator.html")
+})
+app.post("/bmiCalculator",function (req,res) {
+    var weigth = Number(req.body.num1);
+    var height = Number(req.body.num2);
+    var result = weigth*height;
+    res.send("Your BMI is " + result);
+})
+
+
 app.post("/",function (req,res) {
     var num1 = Number(req.body.num1);
     var num2 = Number(req.body.num2);
